2d65e5ccfefe833a81320f6062b72655aee17114
Tony-Ismail <duchemin.tony.dev@gmail.com>
2022-04-18 15:37:30 +0200

    Composant import de média

--- build/11-media/index.js
+++ build/11-media/index.js
@@ -1,12 +1,9 @@
-/******/ (function() { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
+(function() {
+"use strict";
+var __webpack_modules__ = ({
 
-/***/ "./src/11-media/edit.js":
-/*!******************************!*\
-  !*** ./src/11-media/edit.js ***!
-  \******************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+"./src/11-media/edit.js":
+(function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
